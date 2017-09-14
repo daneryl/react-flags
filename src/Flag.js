65c5afe62c0b24ec4794e5738f6e9434e311e230
daneryl <daneryl@gmail.com>
2017-09-14 15:12:55 +0200

    Flag is now an Es6 class, defining proptypes using the new standalone prop-types module

--- src/Flag.js
+++ src/Flag.js
@@ -1,73 +1,17 @@
-import React, {PropTypes} from "react";
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
+
 import availableFlags from "./flags.json5";
 import find from "lodash/collection/find";
 import countries from "!filter-loader?cca2,cca3!world-countries/countries.json";
 
-export default React.createClass({
-  /**
-   * React properties
-   */
-
-  propTypes: {
-    // Alternative text of the flag <img> HTML tag.
-    alt: PropTypes.string,
-
-    // Base path to the content of /vendor
-    basePath: PropTypes.string,
-
-    // Country or region for this flag. (Legacy)
-    country: PropTypes.string,
-
-    // File format of the flag.
-    format: PropTypes.oneOf(["png", "icns", "ico"]),
-
-    // Height of the flag <img> HTML tag.
-    height: PropTypes.number,
-
-    // Name of country or region for this flag. (Legacy)
-    name: PropTypes.string,
-
-    // Size of the PNG country flag
-    pngSize: PropTypes.oneOf([16, 24, 32, 48, 64]),
-
-    // Shiny or Flat
-    shiny: PropTypes.bool,
-
-    // Width of the flag <img> HTML tag.
-    width: PropTypes.number,
-  },
-
-  /**
-   * React lifecycle
-   */
-
-  getDefaultProps() {
-    return {
-      basePath: "/img/flags",
-
-      country: "_unknown",
-
-      name: null,
-
-      format: "png",
-
-      pngSize: 32,
-
-      shiny: false,
-
-      width: null,
-
-      height: null,
-
-      alt: null
-    };
-  },
+export default class Flag extends Component {
 
   // Get information about a country using the alpha-3 ISO code.
   cca3To2(cca3) {
     let country = find(countries, {"cca3": cca3});
     return country ? country.cca2 : "_unknown";
-  },
+  }
 
   /**
    * React render
@@ -113,4 +57,57 @@ export default React.createClass({
       />
     );
   }
-});
+};
+
+Flag.defaultProps = {
+  basePath: "/img/flags",
+
+  country: "_unknown",
+
+  name: null,
+
+  format: "png",
+
+  pngSize: 32,
+
+  shiny: false,
+
+  width: null,
+
+  height: null,
+
+  alt: null
+};
+
+/**
+ * React properties
+ */
+
+Flag.propTypes = {
+  // Alternative text of the flag <img> HTML tag.
+  alt: PropTypes.string,
+
+  // Base path to the content of /vendor
+  basePath: PropTypes.string,
+
+  // Country or region for this flag. (Legacy)
+  country: PropTypes.string,
+
+  // File format of the flag.
+  format: PropTypes.oneOf(["png", "icns", "ico"]),
+
+  // Height of the flag <img> HTML tag.
+  height: PropTypes.number,
+
+  // Name of country or region for this flag. (Legacy)
+  name: PropTypes.string,
+
+  // Size of the PNG country flag
+  pngSize: PropTypes.oneOf([16, 24, 32, 48, 64]),
+
+  // Shiny or Flat
+  shiny: PropTypes.bool,
+
+  // Width of the flag <img> HTML tag.
+  width: PropTypes.number,
+}
